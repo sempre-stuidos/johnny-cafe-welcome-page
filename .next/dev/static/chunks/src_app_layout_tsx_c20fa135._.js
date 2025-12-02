709a6f0d36923f636c5d013f4643c547e4a6a200
yolxander.jaca <yolxander.jaca@humancontact.com>
2025-12-02 16:51:40 -0500

    setup - updated typography wit hthe actully style from figma

--- .next/dev/static/chunks/src_app_layout_tsx_c20fa135._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__98497479._.css",
-  "static/chunks/node_modules__pnpm_eb0f4b85._.js",
-  "static/chunks/src_89d7fb25._.js"
-],
-    source: "dynamic"
-});
